c8ad55e7958913a6f2f07dc7cb0c2d6d7505a554
NatashaB-randao <natashabrandao57@hotmail.com>
2023-03-23 19:40:57 -0300

    Layout

--- CalculatorTutorial/CalculatorTutorial/ViewController.swift
+++ CalculatorTutorial/CalculatorTutorial/ViewController.swift
@@ -8,12 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var CalculatorWorking: UILabel!
+    @IBOutlet weak var CalculatorResults: UILabel!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func equalsTap(_ sender: Any) {
+    }
+    
+    @IBAction func allClearTap(_ sender: Any) {
+    }
+    
+    @IBAction func backTap(_ sender: Any) {
+    }
+    
+    @IBAction func percentTap(_ sender: Any) {
+    }
+    
+    @IBAction func divideTap(_ sender: Any) {
+    }
+    
+    @IBAction func timesTap(_ sender: Any) {
+    }
+    
+    @IBAction func minusTap(_ sender: Any) {
+    }
+    
+    @IBAction func plusTap(_ sender: Any) {
+    }
+    
+    @IBAction func decimalsTap(_ sender: Any) {
+    }
+    
+    @IBAction func zeroTap(_ sender: Any) {
+    }
+    
+    @IBAction func oneTap(_ sender: Any) {
+    }
+    
+    @IBAction func twoTap(_ sender: Any) {
+    }
+    
+    @IBAction func threeTap(_ sender: Any) {
+    }
+    
+    @IBAction func fourTap(_ sender: Any) {
+    }
+    
+    @IBAction func fiveTap(_ sender: Any) {
+    }
+    
+    @IBAction func sixTap(_ sender: Any) {
+    }
+    
+    @IBAction func sevenTap(_ sender: Any) {
+    }
+    
+    @IBAction func eightTap(_ sender: Any) {
+    }
+    
+    @IBAction func nineTap(_ sender: Any) {
+    }
 }
+
 
